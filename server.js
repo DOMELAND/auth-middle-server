e81aafa2238a15dbc6498bc8b16ca159cd4d3201
maxatbj <iamliqiang@gmail.com>
2022-04-23 21:35:16 +0800

    none token test

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post('/web3/changepass',  async (req, res) => {
     .then( function (resp) {
          console.log(resp.status);
          console.log(resp.statusText);
-         console.log(resp.json());
+         console.log(resp.text());
          res.status(resp.status).json({ message: resp.statusText });
     })
 
